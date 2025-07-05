cefc5073bb433b488997d39abd552564bab1ff56
saichaitanya292 <saichaitanya292@gmail.com>
2025-07-05 23:43:18 +0530

    reports

--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -9,13 +9,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.By;
 import java.time.Duration;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class GoogleTest {
 
     WebDriver driver;
+    ExtentReports extent;
+    ExtentTest test;
 
     @BeforeClass
     public void setUp() {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/ExtentReports/index.html");
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        test = extent.createTest("Google Title Test");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=chrome");
         options.addArguments("--no-sandbox");
@@ -29,17 +39,21 @@ public class GoogleTest {
     @Test
     public void testGoogle() {
         driver.get("https://www.google.com");
+        test.info("Navigated to Google");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
 
         String title = driver.getTitle();
+        test.info("Page title: " + title);
         System.out.println("Title: " + title);
         assert title.contains("Google");
+        test.pass("Title contains 'Google'");
     }
 
     @AfterClass
     public void tearDown() {
+        extent.flush();
         if (driver != null) driver.quit();
     }
 }
